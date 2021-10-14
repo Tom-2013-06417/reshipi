a01747a4fd0cfad298ed484a79d54ae34152c2b1
Tom <airbreather7@gmail.com>
2021-10-14 23:12:14 +0800

    Change to japanese copy

--- components/index/Card.tsx
+++ components/index/Card.tsx
@@ -6,8 +6,8 @@ export interface CardProps {
 
 export default function Card(props: CardProps) {
   return (
-    <a href={props.link} className="rounded shadow-md p-4 mb-4">
-      <h2>{props.heading}&rarr;</h2>
+    <a href={props.link} className="rounded-md shadow-md p-4 mb-4">
+      <h2>{props.heading} &rarr;</h2>
       <p>{props.body}</p>
     </a>
   );
