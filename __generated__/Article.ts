a51c11858f46e254b95f005e17fcbb52cc24530a
Tom <airbreather7@gmail.com>
2021-11-21 21:27:04 +0800

    Ignore prettier for generated types

--- __generated__/Article.ts
+++ __generated__/Article.ts
@@ -8,26 +8,26 @@
 // ====================================================
 
 export interface Article_article_recipe_ingredients_amount {
-  readonly __typename: 'IngredientAmount';
-  readonly unit: string;
-  readonly value: number;
+  readonly __typename: "IngredientAmount";
+  readonly unit: string | null;
+  readonly value: string | null;
 }
 
 export interface Article_article_recipe_ingredients {
-  readonly __typename: 'Ingredient';
+  readonly __typename: "Ingredient";
   readonly id: number | null;
   readonly amount: Article_article_recipe_ingredients_amount;
   readonly name: string;
 }
 
 export interface Article_article_recipe_steps {
-  readonly __typename: 'RecipeSteps';
+  readonly __typename: "RecipeSteps";
   readonly id: number | null;
   readonly value: string;
 }
 
 export interface Article_article_recipe {
-  readonly __typename: 'Recipe';
+  readonly __typename: "Recipe";
   readonly title: string;
   readonly description: string;
   readonly ingredients: ReadonlyArray<Article_article_recipe_ingredients>;
@@ -35,7 +35,7 @@ export interface Article_article_recipe {
 }
 
 export interface Article_article {
-  readonly __typename: 'Article';
+  readonly __typename: "Article";
   readonly _id: string;
   readonly title: string;
   readonly seoDescription: string;
