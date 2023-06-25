474a15b878532089fcb227863ad36abc64e952d5
Tom <airbreather7@gmail.com>
2023-06-25 17:57:08 +0800

    Use new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['Assistant', 'sans-serif'],
+      sans: ['Nunito', 'sans-serif'],
     },
     extend: {},
   },
