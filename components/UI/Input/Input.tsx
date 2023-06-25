7e62c135e7d341349a198dad9c5b5a629bbd3461
Tom <airbreather7@gmail.com>
2023-06-25 21:53:24 +0800

    Make labels optional in the input bit

--- components/UI/Input/Input.tsx
+++ components/UI/Input/Input.tsx
@@ -1,18 +1,25 @@
 interface InputProps {
-  label: string;
+  label?: string;
   type?: 'text'
   [key: string]: unknown;
 }
 
-const Input: React.FC<InputProps> = ({ label, type = 'text', ...rest }) => (
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>
+const Input: React.FC<InputProps> = ({ label, type = 'text', ...rest }) => {
+  const { className, ...inputProperties } = rest;
+
+  return (
+    <div className={`mb-4 ${className}`}>
+      { label
+        ? (<label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>)
+        : <></>
+      }
       <input
         className="border border-gray-300 rounded-lg px-3 py-2 w-full focus:outline-none focus:border-blue-500"
         type={type}
-        {...rest}
+        {...inputProperties}
       />
     </div>
-  );
+  )
+};
 
 export default Input;
