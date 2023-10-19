3e4bfbdd4b2c009cb1fe91308d43bbfdc9fb9fde
Tom Lopez <mlopez@freelancer.com>
2023-10-20 01:31:51 +0800

    Make CMS URL env variable public

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,3 @@
 export const CMS_API = process.env.CMS_API || 'http://localhost:3000/api';
-export const CMS_URL = process.env.CMS_URL || 'http://localhost:3000';
+export const CMS_URL =
+  process.env.NEXT_PUBLIC_CMS_URL || 'http://localhost:3000';
