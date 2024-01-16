dc102d4af61a02c2704e0e32e39c61a11b5664a6
Tom <airbreather7@gmail.com>
2024-01-17 01:31:01 +0800

    Use 127.0.0.1 instead of localhost to make it work in mac

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
-export const CMS_API = process.env.CMS_API || 'http://localhost:3000/api';
+export const CMS_API = process.env.CMS_API || 'http://127.0.0.1:3000/api';
 export const MEDIA_URL =
   process.env.NEXT_PUBLIC_MEDIA_URL || 'https://storage.googleapis.com/reshipi-media-dev';
