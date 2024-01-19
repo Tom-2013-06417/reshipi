8b462635dff48e8e2ffbd81e4cdd6f290bf4a070
Tom <airbreather7@gmail.com>
2024-01-20 01:19:58 +0800

    Convert anchor tags to Link

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const BODY_TRUNCATE_LIMIT = 50;
 
@@ -10,7 +11,7 @@ interface Props {
 }
 
 export const Card: React.FC<Props> = ({ heading, body, link, thumbnail }) => (
-  <a href={link} className="flex rounded-md overflow-hidden border mb-4 min-h-[80px] align-middle justify-between">
+  <Link href={link} className="flex rounded-md overflow-hidden border mb-4 min-h-[80px] align-middle justify-between">
     <div className="p-4">
       <span className="text-xl font-sans font-bold">{heading}</span>
       { body ? <p className="font-light ">{body.slice(0, BODY_TRUNCATE_LIMIT)}</p> : <></>}
@@ -21,5 +22,5 @@ export const Card: React.FC<Props> = ({ heading, body, link, thumbnail }) => (
       height={150}
       alt=""
     /> : <></> }
-  </a>
+  </Link>
 );
